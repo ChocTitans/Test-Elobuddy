4f40bcdae48c6e1a3266020a3fbcb12381d3b564
ChocTitans <sampcfrpg@live.fr>
2017-01-15 23:14:44 +0000

    Project elo
    elotest

---
+++ Menus.cs
@@ -0,0 +1,41 @@
+﻿using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Menus
+    {
+        public static Menu FirstMenu;
+        public static Menu ComboMenu;
+        public static Menu DrawingsMenu;
+
+        public static void CreateMenu()
+        {
+            FirstMenu = MainMenu.AddMenu(Player.Instance.ChampionName, Player.Instance.ChampionName.ToLower() + "Template");
+            ComboMenu = FirstMenu.AddSubMenu("Titans EX");
+            DrawingsMenu = FirstMenu.AddSubMenu("Drawings Ex");
+
+            //TEST COMBO
+            ComboMenu.AddGroupLabel("Combo Settings");
+            ComboMenu.Add("Q", new CheckBox("Use Q"));
+            ComboMenu.Add("W", new CheckBox("Use W"));
+            ComboMenu.Add("E", new CheckBox("Use E"));
+            ComboMenu.Add("R", new CheckBox("Use R"));
+
+            //Drawings
+            DrawingsMenu.AddGroupLabel("Drawings Settings");
+            DrawingsMenu.Add("Q", new CheckBox("Draw Q"));
+            DrawingsMenu.Add("W", new CheckBox("Draw W"));
+            DrawingsMenu.Add("E", new CheckBox("Draw E"));
+            DrawingsMenu.Add("R", new CheckBox("Draw R"));
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu.Values;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        public static void OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.Hero != Champion.Jhin) return;
+            Chat.Print("Hello Jhin");
+            Menus.CreateMenu();
+            Game.OnTick += OnTick;
+        }
+
+        public static void OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo)) ComboExecute();
+        }
+    }
+}
